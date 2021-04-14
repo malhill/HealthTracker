1c56d98abeac6af7baaf1e3ec7cfcd80564fab44
malhill <74620676+malhill@users.noreply.github.com>
2021-04-14 13:54:09 -0400

    updated router and models

---
+++ models/model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+
+  exercise: 
+  [{
+
+    type: {
+        type: String,
+        trim: true,
+        required: "Exercise type is required",
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: "Name of excerise is required",
+    }
+  }],
+
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+  },
+
+  userCreated: {
+    
+  }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
